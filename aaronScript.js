2ae8c69df5d866ed5e4009fadd31f4cff719e095
akaneti <aaronkaneti@gmail.com>
2019-03-22 13:39:48 -0500

    Took unnessecary scripts out. Added quizing functionality.

---
+++ aaronScript.js
@@ -0,0 +1,13 @@
+
+function pickedInKey() {
+  document.getElementById("picked").innerHTML = "You picked the inKey output. :(";
+}
+
+
+function pickedRandom() {
+  document.getElementById("picked").innerHTML = "You picked the random output. :(";
+}
+
+function pickedUs() {
+  document.getElementById("picked").innerHTML = "You picked the our output!! :)";
+}
